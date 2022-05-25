5c55eeebd649ddaa2087e686b1a2dcffc0887a16
Liam Gentile <liamgentile@Liams-MacBook-Pro.local>
2022-05-25 13:49:32 -0400

    adjusted font colours

--- src/App.vue
+++ src/App.vue
@@ -30,15 +30,18 @@ html {
 }
 
 /* global styling for headers and paragraph  */
-@import url('https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300&family=Roboto:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400&family=Roboto:wght@700&display=swap');
 
 h1, h3, h3, h4, h5, h6 {
   font-family: 'Roboto', sans-serif;
+  color: #222325
 }
 
 p, footer {
   font-family: 'Libre Franklin', sans-serif;
+  color: #626a75;
 }
+
 
 
 </style>
