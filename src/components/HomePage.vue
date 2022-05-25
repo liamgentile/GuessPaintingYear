8a5a0a850603eba1bd86a6bd8015906d442358bc
Liam Gentile <liamgentile@Liams-MacBook-Pro.local>
2022-05-25 13:28:03 -0400

    added placeholder label and button to HomePage component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home" aria-hidden="true">
     <h1>Guess the Painting Year!</h1>
-
+    <h3>Test your intuition for painting dating</h3>
+    <label for=""></label>
+    <button></button>
     <footer>A simple web app powered by the Art Institute of Chicago</footer>
   </div>
 </template>
