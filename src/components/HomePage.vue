4cf8cb8bb529f156d392eae6e5710fba49cfcfe6
Liam Gentile <liamgentile@Liams-MacBook-Pro.local>
2022-05-26 11:46:30 -0400

    updated button and label, added placeholder data elements for painting information

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,8 +2,8 @@
   <div class="home" aria-hidden="true">
     <h1>Guess the Painting Year!</h1>
     <h3>Test your intuition for painting dating</h3>
-    <label for=""></label>
-    <button></button>
+    <label for="#random-painting-button" class="button-label">Fetch a Random Painting</label>
+    <button type="button" class="get-painting-button" id="random-painting-button">Get</button>
     <footer>A simple web app powered by the Art Institute of Chicago API</footer>
   </div>
 </template>
@@ -11,11 +11,37 @@
 <script>
 export default {
   name: "HomePage",
+  data() {
+    return {
+      paintingDate: null,
+      painterName: "",
+      paintingName: "",
+      randomId: null
+    }
+  }
 };
+
 </script>
 
 <style scoped>
 
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h3 {
+  margin-bottom: 1.5rem;
+}
+.button-label {
+  margin-bottom: 1rem;
+}
+.get-painting-button {
+  width: 5rem;
+  margin: auto
+  
+}
 footer {
   position:absolute;
   bottom:0;
