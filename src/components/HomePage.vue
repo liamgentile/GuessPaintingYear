0c559661e972d821cf3c998d628c619a7ec02c1b
Liam Gentile <liamgentile@Liams-MacBook-Pro.local>
2022-05-26 16:24:07 -0400

    added conditional logic to show date guess and image only when the image path is not empty

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -9,20 +9,36 @@
       type="button"
       class="get-painting-button"
       id="random-painting-button"
+      @click="submit"
     >
       Generate
     </button>
-    <img :src="imagePath" :alt="altText" />
-    <label class="input-label" for="#date-input"
+    <img :src="imagePath" :alt="altText" v-if="imageShown"/>
+    <label class="input-label" for="#date-input" v-if="imageShown"
       >Input your guess below for what year the above painting was made.</label
     >
-    <form action="" v-on:submit.prevent="submit">
-      <input id="date-input" class="date-input" type="text" v-model="paintingDateGuess" placeholder="2000" @click="resetValidations" />
-      <button type="button" class="guess-button" id="guess-button" @click="validate">Guess</button>
-    </form>
-    
-    <p class="exception-message"> {{ exception }} </p>
-  
+
+    <input
+      id="date-input"
+      class="date-input"
+      type="text"
+      v-model="paintingDateGuess"
+      placeholder="2000"
+      @click="resetValidations"
+      v-if="imageShown"
+    />
+    <button
+      type="submit"
+      class="guess-button"
+      id="guess-button"
+      @click="validate"
+      v-if="imageShown"
+    >
+      Guess
+    </button>
+
+    <p class="exception-message">{{ exception }}</p>
+
     <footer>Powered by the Art Institute of Chicago API</footer>
   </div>
 </template>
@@ -36,49 +52,69 @@ export default {
       paintingDate: 0,
       artistName: "",
       paintingName: "",
-      // setting a random id to fetch in a valid range 
-      randomId: Math.floor(Math.random() * 90000) + 12000,
+      // setting a random id to fetch in a valid range
+      randomId: Math.floor(Math.random() * 90000) * 1000 + 12000,
       paintingDateGuess: null,
       imagePath: "",
-      altText: ""
+      altText: "",
     };
+  },
+  computed: {
+    imageShown() {
+      return this.imagePath !== "";
+    }
   },
   methods: {
     validate() {
-      // validations 
-      if (!Number.isInteger(+this.paintingDateGuess) && this.paintingDateGuess) {
-        this.exception = 'Please input a valid date, for example, 1976.';
+      // validations
+      if (
+        !Number.isInteger(+this.paintingDateGuess) &&
+        this.paintingDateGuess
+      ) {
+        this.exception = "Please input a valid date, for example, 1976.";
         document.getElementById("date-input").classList.add("invalid-input");
       }
       if (!this.paintingDateGuess) {
-        this.exception = 'Woops, you forgot to fill out the date form.';
+        this.exception = "Woops, you forgot to fill out the date form.";
         document.getElementById("date-input").classList.add("incomplete");
       }
     },
     resetValidations() {
       this.exception = "";
-      document.getElementById("date-input").classList.remove('invalid-input', "incomplete");
+      document
+        .getElementById("date-input")
+        .classList.remove("invalid-input", "incomplete");
     },
     async submit() {
       if (this.exception === "") {
-      fetch(`https://api.artic.edu/api/v1/artworks/${this.randomId}?fields=title,artist_title,date_end,alt_text,image_id`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((response) => {
-          console.log(response);
+        fetch(
+          "https://api.artic.edu/api/v1/artworks/12000?fields=title,artist_title,date_end,alt_text,image_id",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            // retrieving the necessary data from the api response
+            this.imagePath =
+              response.config.iiif_url +
+              "/" +
+              response.data.image_id +
+              "/full/843,/0/default.jpg";
 
-          this.imagePath = response.config.iiif_url + '/' + response.data.image_id + '/full/843,/0/default.jpg';
-          // this.responses.push({
-          //   prompt: "Prompt: ".bold() + this.input_prompt,
-          //   response: "Response: ".bold() + response.choices[0].text,
-          //   time: new Date(),
-          // });
-        });
-    }}
+            this.artistName = response.data.artist_title;
+
+            this.paintingName = response.data.title;
+
+            this.paintingDate = response.data.date_end;
+
+            this.altText = response.data.alt_text;
+          });
+      }
+    },
   },
 };
 </script>
@@ -105,7 +141,10 @@ h3 {
 }
 
 img {
-
+  margin: auto;
+  margin-top: 2rem;
+  height: 20rem;
+  width: 20rem;
 }
 
 .input-label {
@@ -133,18 +172,13 @@ img {
   font-weight: 700;
 }
 .incomplete {
-   border: 3px solid rgb(226, 98, 98);
+  border: 3px solid rgb(226, 98, 98);
 }
 
 .invalid-input {
   border: 3px solid rgb(241, 156, 65);
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
 footer {
   position: absolute;
   bottom: 0;
